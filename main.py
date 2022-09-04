161ecc9cb6c8e770a69beccc50c333459a3756f8
Jake Huang <ji394gpx06@gmail.com>
2022-09-04 18:53:37 +0800

    fix selenuim 4.4.3

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 from selenium import webdriver
+import selenium
 from selenium.webdriver.chrome.options import Options
 
 ChromeDRVIER_PATH = '/usr/local/bin/chromedriver'
@@ -16,14 +17,15 @@ def crowweb():
     driver = webdriver.Chrome(executable_path=ChromeDRVIER_PATH,chrome_options=chrome_options)
 
     driver.get("https://finance.yahoo.com/watchlists/")
-    SP500=driver.find_elements_by_id("marketsummary-itm-0")
+    SP500=driver.findElements(By.id("marketsummary-itm-0"))
     for element in SP500:
         print(element.text)
-    Dow30=driver.find_elements_by_id("marketsummary-itm-1")
+    Dow30=driver.findElements(By.id("marketsummary-itm-1"))
     for element in Dow30:
         print(element.text)
     driver.close()
 if __name__ == '__main__':
     crowweb()
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
